4d6ae75a9dac0b69c952d42ca9ab10c256022b69
Lisa <lisachan.nb@gmail.com>
2017-11-29 01:04:35 -0500

    UI touchups

--- lib/neu_map_web/views/building_view.ex
+++ lib/neu_map_web/views/building_view.ex
@@ -4,4 +4,14 @@ defmodule NeuMapWeb.BuildingView do
   def get_area!(id) do
     NeuMap.Map.get_area!(id)
   end  
+
+  def get_services(nil), do: nil
+
+  def get_services(building_id) do
+    Enum.filter(NeuMap.Map.list_service(), fn(x) -> x.building_id == building_id end)
+  end
+
+  def get_color(service) do
+    NeuMap.Map.get_tag!(service.tag_id).color
+  end
 end
--- lib/neu_map_web/views/tag_view.ex
+++ lib/neu_map_web/views/tag_view.ex
@@ -1,3 +1,9 @@
 defmodule NeuMapWeb.TagView do
   use NeuMapWeb, :view
+
+  def get_services(nil), do: nil
+
+  def get_services(tag_id) do
+    Enum.filter(NeuMap.Map.list_service(), fn(x) -> x.tag_id == tag_id end)
+  end
 end
